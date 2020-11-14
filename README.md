3bab694eda7d6b4e9c795db175c95ab454018189
bringItUpX <brexter@gmx.de>
2020-11-14 10:04:23 +0100

    fix in german script, add script for all languages of the game

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # Micro-Machines-V3-Patch
-Patch for slowing down the speed of the game Micro Machines V3 on modern Windows / Linux(wine) computers.
 
-Run this script with "python3 patch_micro_exe_german.py".
+Patch for slowing down the speed of the game Micro Machines V3 on modern Windows / Linux (wine) computers. This game the produced by Codemasters.
 
-The executable of the game has to be in the same directory. Please make a backup before. The executable of the MMV3 game which is patched is micro.exe (choose german during the installation of the game, because this script is written with that version in mind.) The executable should from the date 12. May 1998. A verifying is running in the python script before running the patch, but there is no backup made.
+Run this script with "python3 patch_micro_exe_german.py" (if you have installed the german version of the game).
+Run this script with "python3 patch_micro_exe_all_languages.py" (if you have installed any language version of the game).
 
-If the patch script runs successfully, copy the file micro.exe back to your game directory (don't overwrite your backup of the orginal file), and start the game normally (you should choose Software Rendered).
 
-In the game then all players are on the road and the light is green (all players can move theire cars) then press the key F7 to more slow down the game and to increase the screen resolution.
+The executable of the game has to be in the same directory like the python script. Please make a backup of the game before you run the script. The executable of the MMV3 game which is patched is micro.exe . The executable should from the date 12. May 1998 (Perhaps other dates works also, but then you need the all_languages script version). A verifying is running in the python script before running the patch, but there is no backup made.
 
-Through the patch you can play the game like in the old times with an Pentium I at 120MHz Clock. I think a better used screen (really full screen) is made only when you run this game on Linux with Wine. But it's not easy to configure, because you have to enable Win32 mode before you create the .wine directory (in the user folder), and you have to set full screen. I used this with Wine 4.0.2 on Linux Mint 19. But also other versions should work. The .wine folder is normally created in WinWoW (Win64) mode automatically then you start wine first and if there is no .wine dirctory in your home folder.
+If the patch script runs successfully, copy the file micro.exe back to your game directory (don't overwrite your backup of the orginal file), and start the game normally (you should choose perhaps Software Rendered in the start menu of Windows).
 
-You can play the game with 6 usb controllers and 2 players at the keyboard - therefore a maximum of 8 players.
+In the game, when all players are on the road and the light is green (all players can move theire cars) then press the key F7 to more slow down the game and to increase the screen resolution.
+
+Through the patch you can play the game like in the old times with a Intel Pentium I processor at 120MHz Clock (on Windows 95/98/ME). I think on nowadays PC's with Windows 10 a better used screen (really full screen) is made only when you run this game on Linux with Wine. But it's not easy to configure, because you have to enable Win32 mode before you create the .wine directory (in the home folder), and you have to set full screen. I used this game with Wine 4.0.2 on Linux Mint 19. But also other versions should work. The .wine folder is normally created in WinWoW (Win64) mode automatically then you start wine first and if there is no .wine dirctory in your home folder.
+
+You can play the game with 6 Usb controllers and 2 players at the keyboard - therefore a maximum of 8 players (better as in the old days with Windows 95 with difficult or no USB and only one Sound card with one game port for a controller). Then you don't have the amount of usb controllers you want, then you can configure the game (before starting the game) for seperating one game controller for two different players. You could also split two game controllers. But I haven't tried this now. I think the executable for configuring the game controllers before starting the game was: mm3joy.exe .
+
+Sometimes under Windows 98/ME the game crashed after perhaps 2 hours of playing. With Windows 95, this was not the case, there it runs without problems. Also as I played the game in 2020 on a Linux PC with Wine 4.0.2 (in Win32 mode not Win64 !) there are no problems - but yeah - you have now Usb.
 
 With the key F10 you can exit the game.
--- patch_micro_exe_all_languages.py
+++ patch_micro_exe_all_languages.py
@@ -7,16 +7,16 @@ end = 2       # 2 sets the reference point at the end of the file
 
 # Syntax: filemicro.seek(offset, from_what), where filemicro is file pointer
 
-patchnum=bytes(b"\x68")
-orig=bytes(b"\x28")
+patchnum=bytes(b"\xc1\x83\xf8\x68")
+orig=bytes(b"\xc1\x83\xf8\x28")
 
 
 print("This Python-Script alter the game Micro Machines V3 to run at speed of an 120MHz Pentium I -")
 print("but on a modern PC in the Year 2020. But in the game you have to additionally slow down")
 print("the speed by pressing the Key 'F7' - also for a higher resolution of the game.")
 print("For this script, the version of the game has to be from the date 12. May 1998.")
-print("This script is made for the german version of the game executable.")
-print("Perhaps it patches also the other languages of the game executable.")
+print("The script should patch all languages of the game executable,")
+print("perhaps also of an other date of the file micro.exe")
 print("------Hack:------")
 print("The game has three occurrences of an assembler 'CMP 0x28, eax' command.")
 print("This CMP command can handle numbers approximately till 0x7E")
@@ -31,7 +31,7 @@ print("If the difference of the two time values is lower than 0x28 the game does
 print("a new frame und returns to Windows.")
 print("If the difference is greater than 0x28 the next frame will be calculated")
 print("------Usage:-----")
-print("To work with this script you have to place the german file micro.exe of the game")
+print("To work with this script you have to place the file micro.exe of any language of the game")
 print("in the same directory like this python script.")
 print("")
 print("------now it runs the script:---------")
@@ -40,49 +40,46 @@ if os.path.exists("micro.exe"):
     file_length_in_bytes = os.path.getsize("micro.exe")
     print("File has this amount of bytes:")
     print(file_length_in_bytes)
-    print("File should have 558080 bytes for the german micro.exe executable")
-    if (file_length_in_bytes == 558080):
-        rightcontent = True
-        with open("micro.exe", "r+b") as filemicro:
-            filemicro.seek(253190, begin)
-            if (filemicro.read(1) == orig):
-                print("1. occurrence in the file has 0x28 -> right")
-            else:
+    print("If it is the german version of the file it should have 558080 bytes for micro.exe executable")
+    offset = 0
+    with open("micro.exe", "r+b") as filemicro:
+        for offset in range(file_length_in_bytes):
+            rightcontent = True
+            filemicro.seek(offset + 0, begin)
+            if (filemicro.read(4) != orig):
                 rightcontent = False
-            filemicro.seek(253224, begin)
-            if (filemicro.read(1) == orig):
-                print("2. occurrence in the file has 0x28 -> right")
-            else:
+            filemicro.seek(offset + 34, begin)
+            if (filemicro.read(4) != orig):
                 rightcontent = False
-            filemicro.seek(253272, begin)
-            if (filemicro.read(1) == orig):
-                print("3. occurrence in the file has 0x28 -> right")
-            else:
+            filemicro.seek(offset + 82, begin)
+            if (filemicro.read(4) != orig):
                 rightcontent = False
-            writepatch = True
-            if (rightcontent == True):
-                print("it is the right micro.exe -> now patching the file.")
-                filemicro.seek(253190, begin)
-                if filemicro.write(patchnum) == 1:
-                    print("has changed the 1. occurrence")
-                else:
-                    writepatch = False
-                filemicro.seek(253224, begin)
-                if filemicro.write(patchnum) == 1:
-                    print("has changed the 2. occurrence")
-                else:
-                    writepatch = False
-                filemicro.seek(253272, begin)
-                if filemicro.write(patchnum) == 1:
-                    print("has changed the 3. occurrence")
-                else:
-                    writepatch = False
-                if writepatch == True:
-                    print("-----micro.exe successfully patched----------")
-            else:
-                print("I do not patch. This is not the right micro.exe executable")
+            if rightcontent == True:
+                break
 
-    else:
-	    print("I do not patch, wrong file size!")
+        writepatch = True
+        if (rightcontent == True):
+            print("offset of the pattern found. It is at:")
+            print(offset)
+            print("it is the right micro.exe -> now patching the file.")
+            filemicro.seek(offset + 0, begin)
+            if filemicro.write(patchnum) == 4:
+                print("has changed the 1. occurrence")
+            else:
+                writepatch = False
+            filemicro.seek(offset + 34, begin)
+            if filemicro.write(patchnum) == 4:
+                print("has changed the 2. occurrence")
+            else:
+                writepatch = False
+            filemicro.seek(offset + 82, begin)
+            if filemicro.write(patchnum) == 4:
+                print("has changed the 3. occurrence")
+            else:
+                writepatch = False
+            if writepatch == True:
+                print("-----micro.exe successfully patched----------")
+        else:
+            print("I do not patch. The pattern can't be found in the micro.exe executable")
 else:
     print("I do not patch, file is not there!")
